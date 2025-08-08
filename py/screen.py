574fe40a97400be3068dccd58ae0a5914711a3d5
hbarylskyi <hleb.barylskyi@gmail.com>
2025-08-09 00:42:01 +0300

    feat: log original cat size and set fixed resize dimensions

--- py/screen.py
+++ py/screen.py
@@ -31,7 +31,8 @@ def display_on_screen(afr, temperature):
     # Draw a cat
     cat = Image.open("cats/nyan-sit.png").convert("1")
     original_size = cat.size
-    new_size = (original_size[0] // 2, original_size[1] // 2)
+    print(f"Original cat size: {original_size}")
+    new_size = (50 , 36)
     
     resized_cat = cat.resize(new_size, Image.LANCZOS)
     
